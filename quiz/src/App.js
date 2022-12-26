99ad20096a04f6182c5b7a1291ababcf8c8197cc
shivangi <shivangichandra806@gmail.com>
2022-12-26 17:05:14 +0530

    First commit

---
+++ quiz/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComp from './Quiz_components/QuizComp';
+
+function App() {
+  return (
+    <div >
+      <QuizComp />
+    </div>
+  );
+}
+
+export default App;
